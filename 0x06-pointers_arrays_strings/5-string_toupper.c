06c02d21fec6b897ef9e3a9f9677c283cd672d99
ezra <ezrayeneneh1992@gmail.com>
2021-01-02 16:06:55 -0700

    uppercase i used the technique called listing all alphabets

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * string_toupper - converts the string to upper
+ * str: character to be converted
+ * Return: character
+ */
+char *string_toupper(char *str)
+{
+	char i[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char j[] = "abcdefghijklmnopqrstuvwxyz";
+	int k = 0;
+	int l;
+
+	while (str[k] != '\0')
+	{
+		for (l = 0; l < 26; l++)
+		{
+			if (str[k] == j[l])
+			{
+				str[k] = i[l];
+			}
+		}
+		k++;
+	}
+	return (str);
+}
