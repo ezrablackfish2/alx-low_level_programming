5c9e058aa09b3c5e7a1b46a40c6da898a8e25d14
ezra <ezrayeneneh1992@gmail.com>
2021-01-07 16:21:32 -0700

    the trick again is to return the remaining string you use address + iterator

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_strchr - locates a character required in a given string
+ * @s: the string given
+ * @c: the charactee we must locate and identify
+ * Return: the remaining string
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return ((s + i));
+		}
+	}
+	if (s[i] == '\0')
+	{
+		return (NULL);
+	}
+	return (s);
+}
