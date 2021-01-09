4af7ada67550351c15df4c06e1b2d12e17bd56d2
ezra <ezrayeneneh1992@gmail.com>
2021-01-09 15:52:26 -0700

    use recursion every initialization of i but use summation of iterator

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: the string size for it to be taken
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s == '\0')
+	{
+		return (i);
+	}
+	i++;
+	return(i + _strlen_recursion(s + 1));
+}
+
