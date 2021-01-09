d9125e0fa5caf8ff33f52a5898860f2fb24ad732
ezra <ezrayeneneh1992@gmail.com>
2021-01-09 15:35:20 -0700

    the trick is that print the string after all recursion reaches last point

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - prints string in reverse
+ * @s: the string to be reversed and printed
+ * Return: nothing
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
