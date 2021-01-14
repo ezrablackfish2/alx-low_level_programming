1817268c974dd9d2f01f7d3c445e0ae00087ed7a
ezra <ezrayeneneh1992@gmail.com>
2021-01-14 16:20:15 -0700

    use array address for malloc change

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include<stdlib.h>
+/**
+ * *create_array - creates array but dynamically with given size
+ * @size: the size of array required
+ * @c: the character that is assigned to each array values
+ * Return: the pointer of array created
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i;
+
+	arr = malloc(sizeof(char) * size);
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+	arr[i] = '\0';
+	return (arr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+this repository is responsile for learning about malloc and free memory allocation
