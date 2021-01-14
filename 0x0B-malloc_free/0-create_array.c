7f5e2957c38bda32d8bbdb0ba9cc522b482504fb
ezra <ezrayeneneh1992@gmail.com>
2021-01-14 16:27:41 -0700

    forgot to make mallocbelow NULL condition

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,11 +11,11 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 	unsigned int i;
 
-	arr = (char *) malloc(sizeof(char) * size);
 	if (size == 0)
 	{
 		return (NULL);
 	}
+	arr = (char *) malloc(sizeof(char) * size);
 	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
