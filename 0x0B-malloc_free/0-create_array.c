ca6cfacbfd2f6a9bee4f398ac7a21c2bc000502e
ezra <ezrayeneneh1992@gmail.com>
2021-01-14 16:34:22 -0700

    malloc return NULL then return 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,6 +16,10 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 	arr = (char *) malloc(sizeof(char) * size);
+	if (arr == NULL)
+	{
+		return (0);
+	}
 	for (i = 0; i < size; i++)
 	{
 		*(arr + i) = c;
