b94095279094639dedd7bceae0aa2556581135b9
ezra <ezrayeneneh1992@gmail.com>
2021-01-14 16:22:35 -0700

    use array address for malloc change

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 	unsigned int i;
 
-	arr = malloc(sizeof(char) * size);
+	arr = (char *)malloc(sizeof(char) * size);
 	if (size == 0)
 	{
 		return (NULL);
