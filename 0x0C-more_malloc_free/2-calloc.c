e61fb8b1f1e01d8486a2ac03fd8339555db5ad0d
ezra <ezrayeneneh1992@gmail.com>
2021-01-16 10:13:05 -0700

    the trick in string concat is to use two if condtition in 1 loop for concatination

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,6 +2,24 @@
 #include <stdlib.h>
 #include "main.h"
 /**
+ * *_memset - fills memory with a constant byte
+ * @arr: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
+ *
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *arr, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		arr[i] = b;
+	}
+	return (arr);
+}
+/**
  * *_calloc - using malloc allocate memory
  * @nmemb: the array
  * @size : the size of the type of variable
@@ -11,7 +29,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *arr;
 
-	if nmemb == 0 || size == 0)
+	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
