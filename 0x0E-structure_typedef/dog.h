4de072fb8826366ec88440d381f380a77b4fef60
ezra <ezrayeneneh1992@gmail.com>
2021-01-21 14:00:45 -0700

    structure is a basic thing and use -> for further pointer to class instead of .

---
+++ 0x0E-structure_typedef/dog.h
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a class of dog 
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ * Return: the dog
+ */
+struct dog 
+{
+	char *name;
+	double age;
+	char *owner;
+};
+/**
+ * dog_t - typedef for the structure dog
+ */
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
