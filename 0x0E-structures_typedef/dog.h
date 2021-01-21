843ec94652993fe7f491582c01ab6f155749ca92
ezra <ezrayeneneh1992@gmail.com>
2021-01-21 14:28:50 -0700

    needed to add main,h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,30 +1,27 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H
+#define _DOG_H
+
+#include "main.h"
 
 /**
- * struct dog - a class of dog
- * @name: the name of the dog
- * @age: the age of the dog
- * @owner: the owner of the dog
- * Return: the dog
+ * struct dog - info on dog.
+ * @name: dog's name.
+ * @age: age of the dog in float.
+ * @owner: name of the owner of the dog.
+ *
  */
-
 struct dog
 {
 	char *name;
-	double age;
+	float age;
 	char *owner;
 };
-/**
- * dog_t - typedef for the structure dog
- */
 
-typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+/* function for typedef dog_t */
+typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
-
 #endif
