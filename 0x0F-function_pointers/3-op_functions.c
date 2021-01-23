96bc28be48c94ab5bdd9335bb18ebab9d070df1a
ezra <ezrayeneneh1992@gmail.com>
2021-01-23 12:05:38 -0700

    minor errors

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,33 +1,60 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "function_pointers.h"
+#include "3-calc.h"
 
 /**
- * op_add - returns the sum of a adn b
- * op_sub - returns the difference of a and b
- * op_mul - return the product of a and b
- * op_div - returns the division of a by b
- * op_mod - returns the remainder of a by b
- * @a: the first number given
- * @b: the second number given
- * Return: the final opeartion
+ * op_add - runs a addition calculation on a and b;
+ * @a: int value.
+ * @b: int value.
+ *
+ * Return: Resault of math operation.
  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+
+/**
+ * op_sub - runs a subtraction calculation on a and b;
+ * @a: int value.
+ * @b: int value.
+ *
+ * Return: Resault of math operation.
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
+/**
+ * op_mul - runs a multiplication calculation on a and b;
+ * @a: int value.
+ * @b: int value.
+ *
+ * Return: Resault of math operation.
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+
+/**
+ * op_div - runs a division calculation on a and b;
+ * @a: int value.
+ * @b: int value.
+ *
+ * Return: Resault of math operation.
+ */
 int op_div(int a, int b)
 {
 	return (a / b);
 }
+
+/**
+ * op_mod - runs a moldules calculation on a and b;
+ * @a: int value.
+ * @b: int value.
+ *
+ * Return: Resault of math operation.
+ */
 int op_mod(int a, int b)
 {
 	return (a % b);
