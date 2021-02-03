095933c344120e23af9e846b49620264442a8630
ezra <ezrayeneneh1992@gmail.com>
2021-02-03 07:57:18 -0700

    using two funcitons technique

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,16 +1,13 @@
 #include "lists.h"
 
 /**
- * free_listint - frees the list of the nodes in linked list
- * @head: is the head of the node
- * Return: nothing
+ * free_listint - frees all the node in a list.
+ * @head: pointer to a list.
  */
-
 void free_listint(listint_t *head)
 {
-	while (head)
-	{
-		free(head);
-		head = head->next;
-	}
+	if (!head)
+		return;
+	free_listint(head->next);
+	free(head);
 }
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,46 +1,66 @@
 #include "lists.h"
 
-/**
- * insert_nodeint_at_index - adding node at the given position n
- * @head: the head node
- * @idx: the index of the node
- * @n: the number value of n
- * Return: nothing
- */
 
+static listint_t *new_node(const int n);
+/**
+ * insert_nodeint_at_index - function that inserts a new node at a given
+ * position.
+ *
+ * @head: pointer of pointer to a listint_t
+ * @idx: index of the list where the new node should be added
+ * @n: value to set the new node to.
+ *
+ * Return: pointer to the head.
+ */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
-	list_t *new;
-	listint_t *temp = *head;
+	listint_t *temp, *new;
 
-	new = malloc(sizeof(listint_t));
-	if (!new || !head)
+	if (!(*head) && idx == 0)
 	{
-		return (NULL);
-	}
-	new->n = n;
-	new->next = NULL;
-
-	if (idx == 0)
-	{
-		new->next = *head;
-		*head = new;
-		return (new);
+		*head = new_node(n);
+		return (*head);
 	}
 
-	for (i = 0; temp && i < idx; i++)
+	i = 0;
+	temp = *head;
+	while (temp)
 	{
-		if (i == idx - 1)
+		if (idx == 0)
 		{
+			*head = new_node(n);
+			(*head)->next = temp;
+			return (*head);
+		}
+		else if (i == idx - 1)
+		{
+			new = new_node(n);
 			new->next = temp->next;
 			temp->next = new;
 			return (new);
 		}
-		else
-		{
-			temp = temp->next;
-		}
+		temp = temp->next;
+		i++;
 	}
 	return (NULL);
+}
+
+
+/**
+ * new_node - creates a new listint_t node.
+ * @n: number to place inside the node.
+ *
+ * Return: pointer to the new node.
+ */
+static listint_t *new_node(const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	return (new);
 }
