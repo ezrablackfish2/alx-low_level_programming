69c3b73d7ff26644d2fe7ef09c8b249ca4116e6e
ezra <ezrayeneneh1992@gmail.com>
2021-02-03 08:03:04 -0700

    old methods were great

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,44 +1,34 @@
 #include "lists.h"
 
-static listint_t *new_node(const int n);
-
 /**
- * add_nodeint - add a node at the head of a list.
- * @head : pointer of a pointer to a list.
- * @n : value to set the new list to.
+ * add_nodeint_end - adds a node at the end of a linked list
+ * @head: pointer to the first element in the list
+ * @n: data to insert in the new element
  *
- * Return: pointer to a pointer of listint_t
+ * Return: pointer to the new node, or NULL if it fails
  */
-listint_t *add_nodeint(listint_t **head, const int n)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new;
-
-	if (!(*head))
-	{
-		*head = new_node(n);
-		return (*head);
-	}
-
-	new = new_node(n);
-	new->next = *head;
-	*head = new;
-	return (*head);
-}
-
-/**
- * new_node - creates a new listint_t node.
- * @n: number to place inside the node.
- *
- * Return: pointer to the new node.
- */
-static listint_t *new_node(const int n)
-{
-	listint_t *new;
+	listint_t *temp = *head;
 
 	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
+
 	new->n = n;
 	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = new;
+
 	return (new);
 }
