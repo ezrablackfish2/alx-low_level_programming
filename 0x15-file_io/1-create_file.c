86e6ca6e123788bac24f30a550aee12edec64c11
ezra <ezrayeneneh1992@gmail.com>
2021-02-11 15:29:50 -0700

    the trick is using O-CREATE AND O_TRUNC

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,8 +19,9 @@ int create_file(const char *filename, char *text_content)
 	{
 		for (l = 0; text_content[l];)
 		{
-			len++;
+			l++;
 		}
+	}
 
 	ope = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wri = write(ope, text_content, l);
@@ -30,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	close (ope);
+	close(ope);
 
 	return (1);
 }
