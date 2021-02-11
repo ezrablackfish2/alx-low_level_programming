e9afb0b2e55ca67a75632b3ba7095ef66b18abb5
ezra <ezrayeneneh1992@gmail.com>
2021-02-11 15:57:24 -0700

    trick is to use O_APPEND

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -24,7 +24,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		}
 	}
 
-	ope = open(filename, O_RDWR | O_APPEND | O_TRUNC, 0600);
+	ope = open(filename, O_WRONLY | O_APPEND);
 	wri = write(ope, text_content, l);
 
 	if (ope == -1 || wri == -1)
