dc7367b30b3669bdb0ca6db654400bcc659f509c
ezra <ezrayeneneh1992@gmail.com>
2021-04-11 14:25:53 -0700

    finished

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,7 +18,7 @@ unsigned int binary_to_uint(const char *str)
 	{
 		if (str[o] < '0' || str[o] > '1')
 			return (0);
-		dec = 2 * dec + (str[i] - '0');
+		dec = 2 * dec + (str[o] - '0');
 	}
 
 	return (dec);
