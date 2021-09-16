a97814ef298ac7055a0158f14eaeacea12848011
ezra <ezrayeneneh1992@gmail.com>
2021-09-16 13:59:27 -0800

    wiaw

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,12 @@
+#include "search_algos.h"
+
+int linear_search(int *array, size_t size, int value) {
+	size_t i;
+	for (i = 0; i < size - 1; i++) {
+		if (array[i] == value) {
+			return i;
+		}
+	}
+	i = -1;
+	return i;
+}
