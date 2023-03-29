6db3f61c838290edd025d2a4e3eb8726f7292ee6
ezra <ezrayeneneh1992@gmail.com>
2023-03-29 05:36:11 -0700

    the trick is to use variables for the value of array value

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings and gives and integer value
+ * @s1: first string to be compared
+ * @s2: second string to be compared
+ * Return: integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i, j, k;
+
+	if (s1[0] == s2[0])
+	{
+		return (0);
+	}
+	else if  (s1[0] > s2[0])
+	{
+		i = 0;
+		j = s1[0];
+		k = s2[0];
+		while (j > k)
+		{
+			j--;
+			i++;
+		}
+		return (i);
+	}
+	else if (s2[0] > s1[0])
+	{
+		i = 0;
+		j = s1[0];
+		k = s2[0];
+		while (k > j)
+		{
+			k--;
+			i++;
+		}
+		return (-i);
+	}
+}
