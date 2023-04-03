e3af85eb50b24606c73d4cbe5318daaf1581acfe
ezra <ezrayeneneh1992@gmail.com>
2023-04-03 08:15:46 -0700

    use return i + 1

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strspn - use to find the length of a string before the
+ * first identified character
+ * @s: the string which is to be counted
+ * @accept: the character that gives the case for count to begin
+ * Return: length
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (accept[0] == s[i])
+		{
+			return ((i + 1));
+		}
+	}
+	return (0);
+}
