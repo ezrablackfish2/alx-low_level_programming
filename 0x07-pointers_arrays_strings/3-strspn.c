3601ccdbaa0a1111c544b5aaa4bea1e8cc032056
ezra <ezrayeneneh1992@gmail.com>
2023-04-03 08:20:17 -0700

    using the technique tactic

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,21 +1,44 @@
 #include "main.h"
-/**
- * _strspn - use to find the length of a string before the
- * first identified character
- * @s: the string which is to be counted
- * @accept: the character that gives the case for count to begin
- * Return: length
- */
-unsigned int _strspn(char *s, char *accept)
-{
-	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+/**
+ * _strspn - Locates a character in a string
+ * @s: This is the main C string to be scanned.
+ * @accept: This is the string containing the list of characters to match in s
+ *
+ * Return: return count
+ **/
+
+unsigned int _strspn(char *s, char *accept)
+
+{
+	int i, j;
+	int count = 0;
+	char *str1, *str2;
+
+	str1 = s;
+	str2 = accept;
+
+	i = 0;
+	while (str1[i] != '\0') /*Declaring WHILE *s */
 	{
-		if (accept[0] == s[i])
+		j = 0;
+		while (str2[j] != '\0') /*Declaring WHILE *accept*/
 		{
-			return ((i + 1));
+			if (str2[j] == str1[i]) /*Evaluate condition*/
+			{
+				count++; /*count number*/
+				break;
+			}
+
+			j++;    /*add j+1*/
 		}
+		if (s[i] != accept[j]) /*If aren't equals*/
+		{
+			break;
+		}
+
+		i++; /*add x+1*/
 	}
-	return (i + 1);
+
+	return (count); /*return the value of count*/
 }
