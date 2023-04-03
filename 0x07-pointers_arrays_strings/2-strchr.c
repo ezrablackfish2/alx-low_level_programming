27aa1cc27ea95259469a6b1aaa16ba59bf6502c5
ezra <ezrayeneneh1992@gmail.com>
2023-04-03 06:50:40 -0700

    windering what the problem is

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,19 +1,21 @@
 #include "main.h"
 /**
- * _strchr - locates a character required in a given string
- * @s: the string given
- * @c: the charactee we must locate and identify
- * Return: the remaining string
+ * _strchr - returns the first occurrence of  the character c
+ * @s: the string the be searched from
+ * @c: the desired character
+ * Return: a pointer to the first occurence
  */
 char *_strchr(char *s, char c)
 {
+
 	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 		{
-			return ((s + i));
+
+			return (s + i);
 		}
 	}
 	return ('\0');
---
+++ 0x07-pointers_arrays_strings/strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strchr - locates a character required in a given string
+ * @s: the string given
+ * @c: the charactee we must locate and identify
+ * Return: the remaining string
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return ((s + i));
+		}
+	}
+	return ('\0');
+}
