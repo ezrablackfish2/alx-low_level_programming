e73993274afc6f7d35b05ceae26e174c688f002c
ezra <ezrayeneneh1992@gmail.com>
2023-04-03 09:02:33 -0700

    never forget to use two pointers

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,25 +1,27 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * _strstr - finds the substring of a pointer
- * @haystack: the whole string that the substring to be located
- * @needle: yje substring that is going to be located
- * Return: the substring
+ * *_strstr - locates a substring
+ * @haystack: string to search in
+ * @needle: substring to look for
+ *
+ * Return: pointer to the beginning of the located substring
+ * or NULL if the substring is not found
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i = 0;
+	int i, j;
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		if (needle[0] == haystack[i])
+		for (j = 0; needle[j] != '\0'; j++)
 		{
-			return (haystack + i);
+			if (haystack[i + j] != needle[j])
+				break;
 		}
+		if (!needle[j])
+			return (&haystack[i]);
 	}
-	if (haystack[i] == '\0')
-	{
-		return (NULL);
-	}
-	return (haystack + i);
+	return (NULL);
 }
