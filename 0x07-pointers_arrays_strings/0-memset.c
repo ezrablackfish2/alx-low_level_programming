794a27cac276f13e0c6f2b2bc53bae9ee22229e5
ezra <ezrayeneneh1992@gmail.com>
2023-04-03 05:45:16 -0700

    the trick is you are asked memory so you should use the address and add the iterate index

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * *_memset - fills memory with constant byte
+ * @s: the pointer to be filled
+ * @b: the value to be filled
+ * @n: the total size of memory in bytes
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+	return (s);
+}
