c817622942af940e65fd110813580f5281f7b7a3
ezra <ezrayeneneh1992@gmail.com>
2023-04-05 06:45:03 -0700

    i think i am getting recursion trick is to use it again again and a base case

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,18 +1,17 @@
 #include "main.h"
-/** _pow_recursion - gives the power of a number
- * @x: the base number to be powered
- * @y: the power of the number
- * Return: the powered number
+
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: value to raise
+ * @y: power
+ *
+ * Return: result of the power
  */
 int _pow_recursion(int x, int y)
 {
-	if (y == 0)
-	{
-		return (1);
-	}
 	if (y < 0)
-	{
 		return (-1);
-	}
+	if (y == 0)
+		return (1);
 	return (x * _pow_recursion(x, y - 1));
 }
