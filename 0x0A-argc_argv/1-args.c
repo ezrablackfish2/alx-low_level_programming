f39d6e42e58503baf43395e553cb9792b99f73bd
ezra <ezrayeneneh1992@gmail.com>
2023-04-07 08:03:11 -0700

    we must use argc -1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the number of arguments given
+ * @argc: the count of the arguments
+ * @argv: the arguments to be printed
+ * Return: integer
+ */
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
