6b2fc66c596d9d891b9d28237d998e3f5b9e5c89
ezra <ezrayeneneh1992@gmail.com>
2023-04-10 08:42:48 -0700

    first free all grid of index and then free te whole grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * free_grid - frees the 2 dimensionla array
+ * @grid: the two dimensionla array
+ * @height: the height of the array
+ * Return: nothing
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL || height == 0)
+	{
+		return;
+	}
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free (grid);
+}
