9496a8440683d0d5b383f9974b6de9557a17f9a0
ezra <ezrayeneneh1992@gmail.com>
2023-04-17 04:30:53 -0700

    betty documentation never forget

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,11 @@
-#ifndef MAIN_H
-#define MAIN_h
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - prints the file name 
+ * Return: always zero
+ */
 int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
 }
-#endif
