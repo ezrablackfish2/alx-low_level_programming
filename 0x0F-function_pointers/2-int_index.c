44f8a838f1a586bec99864d5f6a2d8b8fb014e04
ezra <ezrayeneneh1992@gmail.com>
2023-04-19 02:12:39 -0700

    used return -1

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,12 +12,14 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
-
-	for (i = 0; i < size; i++)
-	{
-		if ((*cmp)(array[i]) == 1)
+	if (array == 1 && cmp == 1)
+	{		
+		for (i = 0; i < size; i++)
 		{
-			return (i);
+			if ((*cmp)(array[i]) == 1)
+			{
+				return (i);
+			}
 		}
 	}
 	return(-1);
