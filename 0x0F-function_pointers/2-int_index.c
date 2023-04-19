a81e0fb28532969c4531d2e87f52cc606ea29f48
ezra <ezrayeneneh1992@gmail.com>
2023-04-19 02:18:35 -0700

    compilation error == 1

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,14 +1,15 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "function_pointers.h"
 
 /**
- * int_index - finds the index of the array that
- * @size: the size of the array
- * @cmp: the pointer that points to a given comparision
- * Return: an integer answer
+ * int_index - earches for an integer
+ * @array: array to search in
+ * @size: size of the array
+ * @cmp: pointer to the comparing function
+ *
+ * Return: index of the first element for which
+ * the cmp function does not return 0, or -1 if no match is found
+ * or size is negative
  */
-
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
@@ -17,11 +18,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		for (i = 0; i < size; i++)
 		{
-			if ((*cmp)(array[i]) == 1)
-			{
+			if (cmp(array[i]) != 0)
 				return (i);
-			}
 		}
 	}
+
 	return (-1);
 }
