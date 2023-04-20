a90d926f4727ed98ae2d7d5fbd36db2cad3e9bf5
ezra <ezrayeneneh1992@gmail.com>
2023-04-20 04:59:23 -0700

    you must first list variadic at the top and never forget to use a character type in vardiac arg

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,26 +1,28 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
+
 /**
- * print_numbers - prints numbers only in variadic functions
- * @separator: is the string to be printed between numbers
- * @n: the number of strings passed to functions
- * Return: nothing
+ * print_numbers - prints numbers given as parameters
+ * @separator: string to be printed between numbers
+ * @n: number of integers passed to the function
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list valist;
 	unsigned int i;
+	va_list list;
 
-	va_start(valist, n);
+	va_start(list, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(valist, int));
-		if (i < n && separator)
-		{
-			printf("%s",separator);
-		}
+		if (!separator)
+			printf("%d", va_arg(list, int));
+		else if (separator && i == 0)
+			printf("%d", va_arg(list, int));
+		else
+			printf("%s%d", separator, va_arg(list, int));
 	}
+
+	va_end(list);
+
 	printf("\n");
-	va_end(valist);
 }
