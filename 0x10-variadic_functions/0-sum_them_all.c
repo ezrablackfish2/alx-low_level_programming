03a482831c5a44b08c8e362ee1aa71145899b48b
ezra <ezrayeneneh1992@gmail.com>
2023-04-20 04:32:34 -0700

    trick is to use va_list valist

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -17,13 +17,13 @@ int sum_them_all(const unsigned int n, ...)
 		return (0);
 	}
 	va_start(valist, n);
-	
+
 	sum = 0;
-	
+
 	for (i = 0; i < n; i++)
 		sum += va_arg(valist, int);
 
 	va_end(valist);
-	
+
 	return (sum);
 }
