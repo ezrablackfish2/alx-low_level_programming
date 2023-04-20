e5d5f3be402cb909948953cb2e315ab3df1471c2
ezra <ezrayeneneh1992@gmail.com>
2023-04-20 05:15:29 -0700

    use a char * and if condition in loop

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,39 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings only is a variadic function
+ * @separator: is the comma between the strings
+ * @n: is the number of strings passed to the argument
+ * Return: nothing
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list vivian;
+	unsigned int i;
+
+	if (!(separator))
+	{
+		return;
+	}
+	va_start(vivian, n);
+	for (i = 0; i < n; i++)
+	{
+		if (!(separator))
+		{
+			printf("%s", va_arg(vivian, char *));
+		}
+		else if (separator && i == 0)
+		{
+			printf("%s", va_arg(vivian, char *));
+		}
+		else
+		{
+			printf(", %s", va_arg(vivian, char *));
+		}
+		va_end(vivian);
+	}
+
+	printf("\n");
+}
+
