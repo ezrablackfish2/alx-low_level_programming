bad941e0bdac489d3b08494d278e43126e285239
ezra <ezrayeneneh1992@gmail.com>
2023-04-27 08:02:22 -0700

    the trick is to use temp file for reversing the first linked list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,27 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
 
 /**
- * add_note_end - adds node at the end
- * @head: the head file 
- * @str: the string to be copied
- * Return: the node
+ * add_node_end - adds a new node at the end of a linked list
+ * @head: double pointer to the list_t list
+ * @str: string to put in the new node
+ *
+ * Return: address of the new element, or NULL if it failed
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new;
 	list_t *temp = *head;
-	int len = 0;
+	unsigned int len = 0;
 
-	while(str[len])
-	{
+	while (str[len])
 		len++;
-	}
-	new = malloc(sizeof(list_t));
 
+	new = malloc(sizeof(list_t));
 	if (!new)
-	{
 		return (NULL);
-	}
 
 	new->str = strdup(str);
 	new->len = len;
@@ -34,9 +33,8 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 
 	while (temp->next)
-	{
 		temp = temp->next;
-	}
+
 	temp->next = new;
 
 	return (new);
