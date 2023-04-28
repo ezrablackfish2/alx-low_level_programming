5b150e87aaccbfc2aacafe131fde746be0bcd1c7
ezra <ezrayeneneh1992@gmail.com>
2023-04-28 05:36:11 -0700

    you have to use temp and chain it with head then make head temp

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * pop_listint - pops out the head node
+ * @head: the head node
+ * Return: the integer node
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int l;
+
+	if(!head || !*head)
+	{
+		return (0);
+	}
+
+	l = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+
+	return (l);
+}
