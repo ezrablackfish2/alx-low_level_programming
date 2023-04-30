a75289589fdc88cd93d65a56a250b58c6cbc7696
ezra <ezrayeneneh1992@gmail.com>
2023-04-29 22:17:08 -0700

    using two funcitons technique

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,33 +1,52 @@
 #include "lists.h"
 
-/**
- * add_nodeint_end - adds node at the end of a linked list
- * @head: the head of the node
- * @n: the number to be given
- * Return: the node
- */
 
+static listint_t *new_node(const int n);
+/**
+ * add_nodeint_end - add a node to the end of a list.
+ * @head: pointer of apointer to the head of a list.
+ * @n: number value to set the new node to.
+ *
+ * Return: pointer to the head of a list.
+ */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *temp = *head, *new;
+	listint_t *new, *temp;
 
-	new = malloc(sizeof(listint_t));
-
-	new->n = n;
-	new->next = NULL;
-
-	if (*head == NULL)
+	if (!(*head))
 	{
-		(*head) = new;
-		return (new);
+		*head = new_node(n);
+		return (*head);
 	}
 
-	while (temp->next)
+	temp = *head;
+	while (temp)
 	{
+		if (!temp->next)
+		{
+			new = new_node(n);
+			temp->next = new;
+			break;
+		}
 		temp = temp->next;
 	}
+	return (*head);
+}
 
-	temp->next = new;
+/**
+ * new_node - creates a new listint_t node.
+ * @n: number to place inside the node.
+ *
+ * Return: pointer to the new node.
+ */
+static listint_t *new_node(const int n)
+{
+	listint_t *new;
 
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
 	return (new);
 }
--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,11 +9,18 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
+	listint_t *temp;
+
 	if (head)
 	{
+		temp = head;
 		while (index > 0)
 		{
-			head = head->next;
+			if (!temp)
+			{
+				return (NULL);
+			}
+			temp = temp->next;
 			index--;
 		}
 	}
@@ -21,5 +28,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	{
 		return (NULL);
 	}
-	return (head);
+	return (temp);
 }
