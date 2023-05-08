db96a3a993b13e7eb9f76706dbc5dc08a21ed0fd
ezra <ezrayeneneh1992@gmail.com>
2023-05-08 05:25:38 -0700

    using open , write and read

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,9 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
